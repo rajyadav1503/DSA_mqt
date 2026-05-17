f7c46cb9c1b37d3a7f6f470d1275bc5320305f53
rajyadav1503 <raajyadav1503@gmail.com>
2026-05-17 22:50:31 +0530

    java files added

---
+++ concatenation.java
@@ -0,0 +1,8 @@
+package DSA_mqt;
+
+public class concatenation {
+     public static void main(String[] args) {
+        System.out.println("Java" + 20 + 30);  //it will treat 20 and 30 as string as it came after a string
+
+    }
+}
