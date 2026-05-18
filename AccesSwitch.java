59541f12ecf83ae24f7bd79548713d9386ff89f4
rajyadav1503 <raajyadav1503@gmail.com>
2026-05-18 23:02:11 +0530

    All commited

---
+++ AccesSwitch.java
@@ -0,0 +1,28 @@
+package DSA_mqt;
+
+import java.util.Scanner;
+
+public class AccesSwitch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String role = sc.nextLine();
+
+        switch (role) {
+            case "admin":
+                System.out.println("Can access anything");
+                break;
+            case "Teacher":
+                System.out.println("Can access all student details");
+                break;
+            case "Student":
+                System.out.println("Can access Syllabus details");
+                break;
+            case "guest":
+                System.out.println("Cannot access details");
+                break;
+            default:
+                System.out.println("Wrong Role");
+                
+        }
+    }
+}
