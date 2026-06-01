ce2c6763d1fe75ae52bc6b3a69a2e67172a8d356
rajyadav1503 <raajyadav1503@gmail.com>
2026-06-02 00:31:30 +0530

    GeeksForGeeks Questions added

---
+++ Arrays/GeeksForGeeksQuestions/reverseAnArray.java
@@ -0,0 +1,24 @@
+package DSA_mqt.Arrays.GeeksForGeeksQuestions;
+
+import java.util.Arrays;
+
+public class reverseAnArray {
+
+    static void reverseArray(int arr[]) {
+        int left = 0;
+        int right = arr.length-1;
+        
+        while(left<right){
+            int temp = arr[left];
+            arr[left]=arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
